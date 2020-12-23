f9bf9a7dc6831c801cdfa790d0d6a0a3346a4217
einiemand-aw-wsl2 <einiemand@outlook.com>
2020-12-23 12:03:46 -0800

    Update recursive_shared_mutex::lock(). Use cv to wait for successful lock acquisition.

--- recursive_shared_mutex/src/recursive_shared_mutex.cpp
+++ recursive_shared_mutex/src/recursive_shared_mutex.cpp
@@ -5,34 +5,33 @@ namespace dts {
 void recursive_shared_mutex::lock() {
     std::thread::id this_id = std::this_thread::get_id();
     std::unique_lock<std::mutex> ulock(mtx_);
-    if (this_id == writer_id_) {
-        // Same thread/writer trying to acquire the shared_mutex again.
-        // Simply increase writer count.
-        ++writer_cnt_;
-    }
-    else {
-        // Another writer or no writer is holding the shared_mutex.
-        // It's also likely that some readers are holding
-        // the shared_mutex.
-        if (writer_id_.has_value()) {
-            // If another writer is holding the shared_mutex,
-            // waiting for it to release.
-            cv_.wait(ulock, [this] {
-                return writer_cnt_ == 0 && reader_map_.empty();
-            });
-        }
-        std::shared_mutex::lock();
-        writer_id_ = this_id;
-        writer_cnt_ = 1;
-    }
+    /*
+     * Proceed only if
+     * Current thread is already a writer
+     * or
+     * There is no writer or reader, and shared_mutex::try_lock() returns
+     * true.
+     *
+     * Use of cv_ guarantees that this function doesn't hold mtx_ forever if it
+     * can't lock successfully.
+     */
+    cv_.wait(ulock, [&] {
+        return this_id == writer_id_ || !writer_id_.has_value() &&
+                                          reader_map_.empty() &&
+                                          std::shared_mutex::try_lock();
+    });
+    writer_id_ = this_id;
+    ++writer_cnt_;
 }
 
 bool recursive_shared_mutex::try_lock() {
     std::thread::id this_id = std::this_thread::get_id();
     std::lock_guard<std::mutex> lkgrd(mtx_);
     if (this_id == writer_id_) {
-        // Same thread/writer trying to acquire the shared_mutex again.
-        // Simply increase writer count.
+        /*
+         * Same thread/writer trying to acquire the shared_mutex again. Simply
+         * increase writer count.
+         */
         ++writer_cnt_;
         return true;
     }
@@ -41,8 +40,7 @@ bool recursive_shared_mutex::try_lock() {
         return false;
     }
     else {
-        // No writer is holding the shared_mutex but some readers
-        // may be.
+        // No writer is holding the shared_mutex but some readers may be.
         bool locked = std::shared_mutex::try_lock();
         if (locked) {
             // Set writer attributes if successfully locked.
@@ -56,21 +54,25 @@ bool recursive_shared_mutex::try_lock() {
 void recursive_shared_mutex::unlock() {
     std::unique_lock<std::mutex> ulock(mtx_);
     if (!writer_id_.has_value()) {
-        // No writer is holding the shared_mutex.
-        // Call unlock() anyway. UB expected.
+        /*
+         * No writer is holding the shared_mutex. Call unlock() anyway. UB
+         * expected.
+         */
         std::shared_mutex::unlock();
     }
     else {
-        // At this point, the shared_mutex must be held by a writer
-        // and writer_cnt_ must be greater than 0.
+        /*
+         * At this point, the shared_mutex must be held by a writer and
+         * writer_cnt_ must be greater than 0.
+         */
         --writer_cnt_;
         if (writer_cnt_ == 0) {
-            // If writer count becomes 0, release the
-            // underlying shared_mutex.
-            // It's likely that we are unlocking in a
-            // different thread from the one where the shared_mutex
-            // has been acquired.
-            // Call unlock() anyway. UB expected.
+            /*
+             * If writer count becomes 0, release the underlying shared_mutex.
+             * It's likely that we are unlocking in a different thread from the
+             * one where the shared_mutex has been acquired. Call unlock()
+             * anyway. UB expected.
+             */
             std::shared_mutex::unlock();
             writer_id_ = std::nullopt;
             // Manually unlock to let cv notify.
