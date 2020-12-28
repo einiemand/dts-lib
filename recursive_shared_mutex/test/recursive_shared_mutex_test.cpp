02d5ce5ea5113cd61e942f3ece9396d128d2a61a
einiemand-aw-wsl2 <einiemand@outlook.com>
2020-12-27 19:13:04 -0800

    Add more tests for recursive_shared_mutex.

--- recursive_shared_mutex/test/recursive_shared_mutex_test.cpp
+++ recursive_shared_mutex/test/recursive_shared_mutex_test.cpp
@@ -52,11 +52,46 @@ void test_try_lock_shared() {
     rsmtx.unlock_shared();
 }
 
+void test_read_after_write() {
+    recursive_shared_mutex rsmtx;
+    std::lock_guard<recursive_shared_mutex> writer(rsmtx);
+    assert(rsmtx.try_lock_shared());
+    rsmtx.unlock_shared();
+
+    std::thread other_writer([&rsmtx] {
+        assert(!rsmtx.try_lock());
+    });
+    std::thread other_reader([&rsmtx] {
+        assert(!rsmtx.try_lock_shared());
+    });
+
+    other_writer.join();
+    other_reader.join();
+}
+
+void test_write_after_read() {
+    recursive_shared_mutex rsmtx;
+    std::shared_lock<recursive_shared_mutex> reader(rsmtx);
+    assert(!rsmtx.try_lock());
+
+    std::thread other_writer([&rsmtx] {
+        assert(!rsmtx.try_lock());
+    });
+    std::thread other_reader([&rsmtx] {
+        assert(rsmtx.try_lock_shared());
+    });
+
+    other_writer.join();
+    other_reader.join();
+}
+
 int main() {
     test_recursiveness_shared();
     test_recursiveness_unique();
     test_try_lock();
     test_try_lock_shared();
+    test_read_after_write();
+    test_write_after_read();
 
     std::cout << "All tests passed\n";
 
